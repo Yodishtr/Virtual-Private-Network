acf10f61bc43f27efac616e21065f3bca2b329b2
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-04-21 09:07:23 -0400

    implementing the aes utils

--- vpn-common/src/main/java/utilities/EncryptConstants.java
+++ vpn-common/src/main/java/utilities/EncryptConstants.java
@@ -1,8 +1,47 @@
 package utilities;
 
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class EncryptConstants {
 
-    // has only static methods
-    public static String aesUtil() {}
+    // AES methods
+    public static SecretKey generateAesKey() throws NoSuchAlgorithmException {
+        KeyGenerator aesKeyGen = KeyGenerator.getInstance("AES");
+        aesKeyGen.init(256);
+        SecretKey aesKey = aesKeyGen.generateKey();
+        return aesKey;
+    }
+
+    public static byte[] generateIv() {
+        byte[] iv = new byte[16];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(iv);
+        return iv;
+    }
+
+    public static byte[] encryptPlainText(byte[] plainText, SecretKey aesKey, byte[] iv) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+        String transformation = "AES/CBC/PKCS5Padding";
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance(transformation);
+        cipher.init(Cipher.ENCRYPT_MODE, aesKey, ivParameterSpec);
+        return cipher.doFinal(plainText);
+    }
+
+    public static byte[] decryptCipherText(byte[] cipherText, SecretKey aesKey, byte[] iv) throws
+            NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        String transformation = "AES/CBC/PKCS5Padding";
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance(transformation);
+        cipher.init(Cipher.DECRYPT_MODE, aesKey, ivParameterSpec);
+        return cipher.doFinal(cipherText);
+    }
 
 }
