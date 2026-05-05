8cfd2d43a68f92f6694ae63c1751ab02dd592f01
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-05-05 10:16:20 -0400

    completed the tests for all the constant encryption helper classes

--- vpn-common/src/test/java/HMACUtilTest.java
+++ vpn-common/src/test/java/HMACUtilTest.java
@@ -1,20 +1,28 @@
 import encryption.HMACUtil;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class HMACUtilTest {
 
+    private SecretKey aesKey;
+
+    @BeforeEach
+    void setAesKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        aesKey = keyGenerator.generateKey();
+    }
+
     @Test
     void testDeriveHmacKey() throws NoSuchAlgorithmException, InvalidKeyException {
-        // AES Key Generation for this method
-        KeyGenerator aesKeyGen = KeyGenerator.getInstance("AES");
-        aesKeyGen.init(256);
-        SecretKey aesKey = aesKeyGen.generateKey();
         byte[] aesKeyArray = aesKey.getEncoded();
 
         // hmac key testing
@@ -25,13 +33,27 @@ public class HMACUtilTest {
 
     @Test
     void testComputeHmacKeySameMessageSoSameHmac() throws NoSuchAlgorithmException, InvalidKeyException {
+        byte[] aesKeyArray = aesKey.getEncoded();
+        byte[] derivedHmacKey = HMACUtil.deriveHMACKey(aesKeyArray);
 
+        byte[] inputText = "hes a married man. with a GOOMAR!".getBytes(StandardCharsets.UTF_8);
+        byte[] firstResult = HMACUtil.computeHMAC(derivedHmacKey, inputText);
+        byte[] secondResult = HMACUtil.computeHMAC(derivedHmacKey, inputText);
+        Assertions.assertArrayEquals(firstResult, secondResult);
     }
 
     @Test
     void testComputeHmacKeyDiffMessageDiffHmac() throws NoSuchAlgorithmException, InvalidKeyException {
         // same key + same message gives same HMAC
         // same key + different message gives different HMAC
+        byte[] aesKeyArray = aesKey.getEncoded();
+        byte[] derivedHmacKey = HMACUtil.deriveHMACKey(aesKeyArray);
+
+        byte[] inputTextFirst = "hes a married man. with a GOOMAR!".getBytes(StandardCharsets.UTF_8);
+        byte[] inputTextSecond = "Think Burger boy!".getBytes(StandardCharsets.UTF_8);
+        byte[] firstResult = HMACUtil.computeHMAC(derivedHmacKey, inputTextFirst);
+        byte[] secondResult = HMACUtil.computeHMAC(derivedHmacKey, inputTextSecond);
+        Assertions.assertFalse(Arrays.equals(firstResult, secondResult));
     }
 
     @Test
@@ -40,5 +62,10 @@ public class HMACUtilTest {
         // returns false if message is changed
         // returns false if signature is changed
         // returns false if key is wrong
+        byte[] hmacKey = "Random hmac key".getBytes(StandardCharsets.UTF_8);
+        byte[] message = "Nice Rack!".getBytes(StandardCharsets.UTF_8);
+        byte[] validSignature = HMACUtil.computeHMAC(hmacKey, message);
+        boolean isValid = HMACUtil.verifyHMAC(hmacKey, message, validSignature);
+        Assertions.assertTrue(isValid);
     }
 }
---
+++ vpn-common/src/test/java/RSAUtilTest.java
@@ -0,0 +1,76 @@
+import encryption.RSAUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.Arrays;
+import java.util.Map;
+
+public class RSAUtilTest {
+
+    private static PublicKey publicKey;
+    private static PrivateKey privateKey;
+
+    @BeforeAll
+    static void keysSetUp() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        publicKey = keyPair.getPublic();
+        privateKey = keyPair.getPrivate();
+    }
+
+    @Test
+    void testGenerateRsaKeys() throws NoSuchAlgorithmException {
+        Map<String, Key> rsaKeysMap = RSAUtil.generateRSAKeys(2048);
+        Assertions.assertTrue(rsaKeysMap.containsKey("PublicKey"));
+        Assertions.assertTrue(rsaKeysMap.containsKey("PrivateKey"));
+        Assertions.assertNotNull(rsaKeysMap.get("PublicKey"));
+        Assertions.assertNotNull(rsaKeysMap.get("PrivateKey"));
+        Assertions.assertEquals("RSA", rsaKeysMap.get("PublicKey").getAlgorithm());
+        Assertions.assertEquals("RSA", rsaKeysMap.get("PrivateKey").getAlgorithm());
+    }
+
+    @Test
+    void testEncryptWithPublicKey() throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] inputBytes = "20 years in the can. Not a peep!".getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedData = RSAUtil.encryptWithPublicKey(inputBytes, publicKey);
+        Assertions.assertNotNull(encryptedData);
+        Assertions.assertFalse(Arrays.equals(inputBytes, encryptedData));
+        Assertions.assertTrue(encryptedData.length > 0);
+    }
+
+    @Test
+    void testDecryptWithPrivateKey() throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] inputBytes = "Whatever happened there?!".getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedData = RSAUtil.encryptWithPublicKey(inputBytes, publicKey);
+        byte[] decryptedData = RSAUtil.decryptWithPrivateKey(encryptedData, privateKey);
+        Assertions.assertNotNull(decryptedData);
+        Assertions.assertArrayEquals(inputBytes, decryptedData);
+    }
+
+    @Test
+    void testDecryptingWithDiffPrivateKey() throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        PrivateKey diffPrivateKey = keyPair.getPrivate();
+        byte[] inputBytes = "This mellifluous gift box".getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedData = RSAUtil.encryptWithPublicKey(inputBytes, publicKey);
+        try {
+            byte[] decryptedData = RSAUtil.decryptWithPrivateKey(encryptedData, diffPrivateKey);
+            Assertions.assertFalse(Arrays.equals(inputBytes, decryptedData));
+        } catch (BadPaddingException e) {
+            Assertions.assertTrue(true);
+        }
+    }
+
+}
