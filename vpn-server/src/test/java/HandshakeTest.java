8400673fdaf77e60a22a6997348671c5ee4aeeee
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-05-16 23:48:29 -0400

    YOOOOOOOOOOOOO IT WORKSgit add .git add .git add .git add .git add .git add .git add .git add .git add .

--- vpn-server/src/test/java/HandshakeTest.java
+++ vpn-server/src/test/java/HandshakeTest.java
@@ -1,11 +1,15 @@
 import Network.HandShakeManager;
+import encryption.EncryptedMessage;
 import encryption.SessionCrypto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import protocol.MessageProtocol;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -22,15 +26,18 @@ public class HandshakeTest {
         Future<SessionCrypto> serverTask = executor.submit((Callable<SessionCrypto>) () -> {
             try {
                 Socket clientSocket = serverSocket.accept();
-                InputStream keyStoreStream = HandshakeTest.class.getClassLoader().
+                InputStream propertiesStream = HandshakeTest.class.getClassLoader().
+                        getResourceAsStream("server.properties");
+                InputStream pkcsStream = HandshakeTest.class.getClassLoader().
                         getResourceAsStream("server-keystore.p12");
                 Properties keyStoreProperties = new Properties();
-                keyStoreProperties.load(keyStoreStream);
+                keyStoreProperties.load(propertiesStream);
                 String keyAlias = keyStoreProperties.getProperty("server.keystore.alias");
                 char[] privateKeyPassword = keyStoreProperties.getProperty("server.keystore.password").toCharArray();
                 byte[] keyStorePassword = keyStoreProperties.getProperty("server.keystore.password").getBytes();
+                // passed the input stream of the pkcs12 file to handshakeHandler object
                 HandshakeHandler serverHandshakeHandler = new HandshakeHandler(clientSocket, keyAlias,
-                        privateKeyPassword, keyStoreStream, keyStorePassword);
+                        privateKeyPassword, pkcsStream, keyStorePassword);
                 SessionCrypto serverSessionCrypto = serverHandshakeHandler.performHandshake();
                 return serverSessionCrypto;
             } catch (Exception e) {
@@ -43,10 +50,57 @@ public class HandshakeTest {
         Socket clientSocket = new Socket("localhost", 8080);
         HandShakeManager clientHandshakeManager = new HandShakeManager(clientSocket);
         SessionCrypto clientSessionCrypto = clientHandshakeManager.handleHandshake();
-        if (serverTask.isDone()) {
-            Assertions.assertNotNull(serverTask.get());
-            Assertions.assertNotNull(clientSessionCrypto);
-        }
+        SessionCrypto result = serverTask.get();
+        Assertions.assertNotNull(result);
+        Assertions.assertNotNull(clientSessionCrypto);
 
+    }
+
+
+    @Test
+    public void testServerDecryption() throws Exception {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ServerSocket serverSocket = new ServerSocket(8080);
+
+        Future<String> serverTask = executor.submit((Callable<String>) () -> {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                InputStream propertiesStream = HandshakeTest.class.getClassLoader().
+                        getResourceAsStream("server.properties");
+                InputStream keystoreStream = HandshakeTest.class.getClassLoader().
+                        getResourceAsStream("server-keystore.p12");
+                Properties keyStoreProperties = new Properties();
+                keyStoreProperties.load(propertiesStream);
+                String keyAlias = keyStoreProperties.getProperty("server.keystore.alias");
+                char[] privateKeyPassword = keyStoreProperties.getProperty("server.keystore.password").toCharArray();
+                byte[] keyStorePassword = keyStoreProperties.getProperty("server.keystore.password").getBytes();
+                HandshakeHandler serverHandshakeHandler = new HandshakeHandler(clientSocket, keyAlias,
+                        privateKeyPassword, keystoreStream, keyStorePassword);
+                SessionCrypto serverSessionCrypto = serverHandshakeHandler.performHandshake();
+                InputStream serverInputStream = clientSocket.getInputStream();
+                MessageProtocol.InboundMessage clientInboundingMessage = MessageProtocol.readMessage(serverInputStream);
+                byte[] clientPayload = clientInboundingMessage.payload();
+                EncryptedMessage receivedEncryptedMessage = EncryptedMessage.deserializeEncryptedMessage(clientPayload);
+                String clientMessage = new String(serverSessionCrypto.decrypt(receivedEncryptedMessage),
+                        StandardCharsets.UTF_8);
+                return clientMessage;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        });
+
+        // Client:
+        Socket clientSocket = new Socket("localhost", 8080);
+        HandShakeManager clientHandshakeManager = new HandShakeManager(clientSocket);
+        SessionCrypto clientSessionCrypto = clientHandshakeManager.handleHandshake();
+        EncryptedMessage encryptedClientMessage = clientSessionCrypto.encrypt("Hello".getBytes(StandardCharsets.UTF_8));
+        OutputStream clientOutputStream = clientSocket.getOutputStream();
+        Integer sendingHello = MessageProtocol.MessageType.DATA.getCode();
+        byte[] serializedEncryptedHello = encryptedClientMessage.serializeEncryptedMessage();
+        MessageProtocol.writeMessage(clientOutputStream, sendingHello.byteValue(), serializedEncryptedHello);
+        String result = serverTask.get();
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("Hello", result);
     }
 }
