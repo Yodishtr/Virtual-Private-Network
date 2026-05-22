08789457947e5b2a7893e95d1aed7de84ac42602
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-05-22 10:04:58 -0400

    added info to server.properties and implemented the database manager

---
+++ vpn-server/src/main/java/db/DatabaseManager.java
@@ -0,0 +1,42 @@
+package db;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseManager {
+
+    private static HikariDataSource hikariDataSource;
+
+    static {
+        HikariConfig hikariConfig = new HikariConfig();
+        Properties serverProperties = new Properties();
+        InputStream serverPropertiesStream = DatabaseManager.class.getClassLoader().
+                getResourceAsStream("server.properties");
+        if (serverPropertiesStream == null) {
+            throw new RuntimeException("Unable to find server properties file");
+        }
+        try {
+            serverProperties.load(serverPropertiesStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        hikariConfig.setJdbcUrl(serverProperties.getProperty("db.url"));
+        hikariConfig.setUsername(serverProperties.getProperty("db.username"));
+        hikariConfig.setPassword(serverProperties.getProperty("db.password"));
+        hikariConfig.setMaximumPoolSize(Integer.parseInt(serverProperties.getProperty("db.pool.size")));
+        hikariConfig.setConnectionTimeout(Integer.parseInt(serverProperties.getProperty("db.connection.timeout")));
+        hikariConfig.setIdleTimeout(Integer.parseInt(serverProperties.getProperty("db.connection.idle.timeout")));
+        hikariConfig.setMaxLifetime(Integer.parseInt(serverProperties.getProperty("db.connection.maxlifetime")));
+        hikariDataSource = new HikariDataSource(hikariConfig);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return hikariDataSource.getConnection();
+    }
+}
