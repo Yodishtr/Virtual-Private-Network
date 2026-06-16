4c1c0a53b403c787cdffd1a66d4845321d969f6f
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-06-16 18:35:03 -0400

    created the additional method in the accesspolicy repo. when will tis end my gawd

--- vpn-server/src/main/java/db/AccessPolicyRepository.java
+++ vpn-server/src/main/java/db/AccessPolicyRepository.java
@@ -59,13 +59,43 @@ public class AccessPolicyRepository implements AccessPolicyRepo{
             return Optional.empty();
         }
         String insertSqlStatment = "INSERT INTO access_policies (user_id, allowed_from_hour, allowed_to_hour, " +
-                "ip_allowlist, bandwidth_limit_mbps) VALUES (?, ?, ?, ?, ?)";
+                "ip_allowlist, bandwidth_limit_mbps) VALUES (?, ?, ?, ?, ?) RETURNING *";
         try (Connection connection = this.dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(insertSqlStatment)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setInt(2, allowedFromHour);
+            preparedStatement.setInt(3, allowedToHour);
 
+            String[] ipStringArray = ipAllowList.toArray(new String[ipAllowList.size()]);
+            Array sqlArray = connection.createArrayOf("inet", ipStringArray);
+            preparedStatement.setArray(4, sqlArray);
+            preparedStatement.setInt(5, bandwidthLimit);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    long accessPolicyId = resultSet.getLong("id");
+                    String userName = resultSet.getObject("user_id").toString();
+                    Integer allowedFromHourInt = resultSet.getInt("allowed_from_hour");
+                    Integer allowedToHourInt = resultSet.getInt("allowed_to_hour");
+                    Array inetArray = resultSet.getArray("ip_allowlist");
+                    List<String> ipAllowListRetrieved = new ArrayList<>();
+                    if (inetArray != null) {
+                        String[] currStringArray = (String[]) inetArray.getArray();
+                        ipAllowListRetrieved = Arrays.asList(currStringArray);
+                    }
+                    Integer bandwidthLimitInt = resultSet.getInt("bandwidth_limit_mbps");
+                    OffsetDateTime createdAtInt = resultSet.getObject("created_at", OffsetDateTime.class);
+                    OffsetDateTime updatedAt = resultSet.getObject("updated_at", OffsetDateTime.class);
+                    AccessPolicy accessPolicyCreated = new AccessPolicy(accessPolicyId, userName, allowedFromHour,
+                            allowedToHour, ipAllowListRetrieved, bandwidthLimitInt, createdAtInt, updatedAt);
+                    return Optional.of(accessPolicyCreated);
+                } else {
+                    return Optional.empty();
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return Optional.empty();
     }
 
     @Override
